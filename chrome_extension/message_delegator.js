c9d03a821a98bfdf80a37bcaec6edbc66b130f53
Oskar Kärrman <05k4r@live.se>
2017-12-30 22:28:04 +0100

    Yo lo

--- chrome_extension/message_delegator.js
+++ chrome_extension/message_delegator.js
@@ -2,15 +2,43 @@ class MessageDelegator {
     constructor(soundcloudModel, groupModel) {
         this.soundcloudModel = soundcloudModel;
         this.groupModel = groupModel;
+        console.log(this);
         this.startListenToMessages();
     }
 
     startListenToMessages() {
-        chrome.runtime.onMessage.addListener(this.testListener);
+        chrome.runtime.onMessage.addListener(this.soundcloudListener.bind(this));
+        chrome.runtime.onMessage.addListener(this.groupListener.bind(this));
     }
 
-    testListener(request, sender, sendResponse) {
-        console.log(request.subject);
-        console.log('över detta var cool musik');
+    soundcloudListener(request, sender, sendResponse) {
+        if (request.subject == 'setCurrentlyPlayingTrack') {
+            this.soundcloudModel.setCurrentlyPlayingTrack(request.track);
+        } else if (request.subject == 'setCurrentlyPlayingRepostedTrack') {
+            this.soundcloudModel.setCurrentlyPlayingRepostedTrack(request.repost);
+        } else if (request.subject == 'setCategoryOnCurrentlyPlayingTrack') {
+            const category = this.groupModel.getCategory(request.categoryId);
+            this.soundcloudModel.setCategoryOnCurrentlyPlayingTrack(category);
+        } else if (request.subject == 'addLabelToCurrentlyPlayingTrack') {
+            const label = this.groupModel.getLabel(request.labelId);
+            this.soundcloudModel.addLabelToCurrentlyPlayingTrack(label);
+        } else if (request.subject == 'removeLabelFromCurrentlyPlayingTrack') {
+            const label = this.groupModel.getLabel(request.labelId);
+            this.soundcloudModel.removeLabelFromCurrentlyPlayingTrack(label);
+        }
+    }
+
+    groupListener(request, sender, sendResponse) {
+        if (request.subject == 'addLabels') {
+            this.groupModel.addLabels(request.labels);
+        } else if (request.subject == 'addCategories') {
+            this.groupModel.addCategories(request.categories);
+        } else if (request.subject == 'getAllCategories') {
+            this.groupModel.getAllCategories()
+                .then(function(categories) {
+                    sendResponse({ categories: categories });
+                });
+            return true;
+        }
     }
 }
--- chrome_extension/wader.js
+++ chrome_extension/wader.js
@@ -1,8 +1,19 @@
 class Wader {
     constructor() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBn753lEWxDjU9E_rCgF7Nrxjt1tdiA4TI',
+            authDomain: 'wader-d8a71.firebaseapp.com',
+            databaseURL: 'https://wader-d8a71.firebaseio.com',
+            projectId: 'wader-d8a71',
+            storageBucket: 'wader-d8a71.appspot.com',
+            messagingSenderId: '68541536183'
+        });
+
         const storageConnection = new FirestoreConnection();
         const soundcloudModel = new FirestoreSoundcloudModel(storageConnection);
         const groupModel = new FirestoreGroupModel(storageConnection);
+        console.log(groupModel);
+        console.log(soundcloudModel);
         this.messageDelegator = new MessageDelegator(soundcloudModel, groupModel);
     }
 }
